59ef8d0ee1f87255204f22c66a862441d9845587
nautilis <bevanzpf@gmail.com>
2018-01-08 13:38:14 +0800

    access location info by ip

--- my-aes-diary/make_new_diary.py
+++ my-aes-diary/make_new_diary.py
@@ -5,6 +5,7 @@ import time
 import os
 import requests
 import re
+import json
 
 def get_url_res(url, headers={}, tries=2, timeout=19):
   '''get response of request'''
@@ -18,28 +19,58 @@ def get_url_res(url, headers={}, tries=2, timeout=19):
     except:
       continue
 
-  return res
+  return res 
+
+def get_location_info():
+  '''get location by ip'''
+  ip_api = "http://ip-api.com/json/?lang=en"
+  api_res = get_url_res(ip_api)
+  if api_res:
+    jobj = api_res.json()
+    country = jobj['country'].encode('utf-8')
+    region_name = jobj['regionName'].encode('utf-8')
+    city = jobj['city'].encode('utf-8')
+    return country, region_name, city 
+
+  return (None for i in range(3)) 
+
+
+def get_weather_info(country, region_name, city):
+  base_url = "https://tianqi.moji.com/weather/{}/{}/{}"
+  weather_url = base_url.format(country, region_name, city)
+  weather_data = r"市今天实况：(?P<weather>\S+\s\S+)，"
+
+  res = get_url_res(weather_url)
+  if res:
+    mobj = re.search(weather_data, res.text.encode('utf-8'))
+    weather = mobj.group('weather')
+    return weather
 
 # create new diary formated by time 
+date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
 name = time.strftime('%Y-%m-%d_%H:%M:%S.md',time.localtime(time.time()))
 command = 'touch {}'.format(name)
 os.system(command)
 print "new file: {} had been created".format(name)
 
 # access weather info 
-weather_url = "https://tianqi.moji.com/weather/china/guangdong/guangzhou"
-weather_data = r"广州市今天实况：(?P<weather>\S+\s\S+)，"
+country, origion_name, city = get_location_info()
+if not (country and origion_name and city):
+  print 'fail to get location info'
+  exit()
 
-res = get_url_res(weather_url)
-if res:
-  mobj = re.search(weather_data, res.text.encode('utf-8'))
-  weather = mobj.group('weather')
-  date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-  print "{}<===>{}".format(date, weather)
+weather = get_weather_info(country.lower(), origion_name.lower(), city.lower()) 
+
+if weather:
+  print "{}<===>{}·{}<===>{}".format(date, origion_name, city, weather)
 
   with open(name, 'w')as f:
-    string = "__date: __{}\r\n__weather: __{}\r\n".format(date, weather)
+    string = "__date: __{}\r\n__weather: __{}\r\n__location: __{}\r\n".format(date, weather, city)
     f.write(string)
+
+else:
+  print 'failed to get weather info'
+  exit()
 
 print "done"
 
